9e796978b374214ebac9e30e81a5e90b72ee908f
SamuelMidgley <midgley.sam@gmail.com>
2024-11-14 17:04:47 +0000

    Move from remix to vite

--- app/context/theme/ThemeContext.ts
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-import { Theme } from "~/types/theme";
-
-interface IThemeContext {
-  theme: Theme;
-  setTheme: (newTheme: Theme) => void;
-}
-
-export const ThemeContext = createContext<IThemeContext | null>(null);
